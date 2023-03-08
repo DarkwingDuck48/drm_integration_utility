f245cd445bd9f235d162d7b5aac034ea9fbc2145
Maxim Britvin <maksbritvin@gmail.com>
2023-03-08 20:13:26 +0300

    start read file function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::{env, path};
+use std::fs;
+
+use std::io::Read;
+
+fn read_file(file: &fs::File) -> String{
+    let mut rdr = encoding_rs_io::DecodeReaderBytesBuilder::new()
+            .encoding(Some(encoding_rs::UTF_16LE))
+            .build(file);
+    let mut content = String::new();
+    rdr.read_to_string(&mut content).unwrap();
+    content
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let file_path = path::Path::new(&args[1]);
+    if !file_path.exists() {
+        panic!("File not exist")
+    }
+    let file = fs::File::open(file_path).unwrap();
+    let file_content = read_file(&file);
+    let all_lines = file_content.lines();
+    let mut i = 0;
+    for line in all_lines {
+        println!("{}", line);
+        i += 1;
+        if i > 20 {
+            break;
+        }
+    }
 }
